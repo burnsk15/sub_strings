6dfbdfc2fcb2e5119da386431259a65a0d2a6833
Kathleen <burnsk15@yahoo.com>
2023-01-18 18:27:06 -0500

    Add loops to work through both arrays

--- sub_strings.rb
+++ sub_strings.rb
@@ -5,16 +5,28 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 # Method to count how many times a string appears in another string
 def scan_for_string(string, substr)
     count = string.scan(/(?=#{substr})/).count
-    results = {}
     results[substr] = count
-    puts results 
+    #puts results 
 end
 # Method to take a string and an array of words and determine how many times those words show up in the string
-def 
-    # Solit the string into an array
+def substrings (string, substr_array)
+    # Split the string into an array using scan to exclude punctuation
+    str_array = string.scan(/[\w']+/)
+    # Create empty hash
+    results = {}
     # Loop through each item in string array
+    str_array.each do |str|
         # Loop through each item in dictionary array
-            # Call scan_for_string
-
+        substr_array.each do |substr|
+            # Count how many times a string appears in another string
+            count = string.scan(/(?=#{substr})/).count
+            # Make sure to only add object to hash if the count is greater than 0
+            if count >= 1
+                results[substr] = count
+            end
+        end
+    end
+    puts results
 end
-scan_for_string("belowlow", "low")
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
